a821c8d646a7b6059c32a34be7866a4e5924d5c4
Maxwell Wang <maxwang@udel.edu>
2024-08-18 21:45:16 -0400

    added fonts and working deployed site

--- src/app/components/fonts.ts
+++ src/app/components/fonts.ts
@@ -1,5 +1,5 @@
 import { Inter } from "next/font/google";
 export const inter= Inter({
     weight:['100', '200', '300', '400', '500', '600', '700', '800', '900'],
-    subsets: ['latin', "greek"]
+    subsets: ['latin', "greek", "cyrillic", "vietnamese"]
 });
