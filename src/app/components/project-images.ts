257c4ac926379f966e36e91ebda649bc82b6f2fd
mwang840 <maxwang@udel.edu>
2025-03-04 13:27:28 -0600

    Added images to current project

--- src/app/components/project-images.ts
+++ src/app/components/project-images.ts
@@ -1,7 +1,31 @@
-export const ytToAudioImg  = ["/projects/DownloadingYtLink.png", "/projects/DirectoryLocation.png", "/projects/DownloadingYtLink.png"];
-export const sudokuCSPImg = ["/projects/sudokuStartingSite.png", "/projects/FilledSudoku9x9.png"];
-export const HenHacks2024Img = ["/projects/TypeEvolve.png", "/projects/TypeEvolveResults.png"];
-export const CISSchedulerImg = ["/projects/Popup.png", "/projects/DegreePlanner.png", "/projects/UDSemesterSelector.png", "/projects/DegreeRequirements.png"]
-export const DECovidImg = ["/projects/CovidData.png", "/projects/DEDayInfections.png", "/projects/CovidDataSvr.png"];
+export const ytToAudioImg = [
+  "/projects/DownloadingYtLink.png",
+  "/projects/DirectoryLocation.png",
+  "/projects/DownloadingYtLink.png",
+];
+export const sudokuCSPImg = [
+  "/projects/sudokuStartingSite.png",
+  "/projects/FilledSudoku9x9.png",
+];
+export const HenHacks2024Img = [
+  "/projects/TypeEvolve.png",
+  "/projects/TypeEvolveResults.png",
+];
+export const CISSchedulerImg = [
+  "/projects/Popup.png",
+  "/projects/DegreePlanner.png",
+  "/projects/UDSemesterSelector.png",
+  "/projects/DegreeRequirements.png",
+];
+export const DECovidImg = [
+  "/projects/CovidData.png",
+  "/projects/DEDayInfections.png",
+  "/projects/CovidDataSvr.png",
+];
 export const CompVisionsImg = ["/projects/cifar100.jpg"];
 export const ytFlaskImg = ["/projects/screenshot.png"];
+export const HenHacks2025Img = [
+  "/projects/CanvasAssistantChat.png",
+  "/projects/CanvasAssistantHome.png",
+  "/projects/CanvasAssistantSidebar.png",
+];
